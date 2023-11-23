6c9ec1908470809c4d007ff2c6a90ad1b9eb1c0b
jinasun <ginasuns60916@gmail.com>
2023-11-23 10:28:57 +0800

    v2

--- src/main/java/com/example/Appliance.java
+++ src/main/java/com/example/Appliance.java
@@ -42,16 +42,17 @@ public class Appliance {
     // TODO: Implement getters for brand, model, and powerUsage
 
     public void displayInfo (){
+        return "The displayInfo method does not return the correct information.";
     }
     // displayInfo Method
     // TODO: Implement a method that returns information about the appliance, including brand, model, and power usage.
     // Check the tests for more details
 
-    public void isEcoFriendly(){
-        if (this.powerUsage <= 400){
-            println("The displayInfo method does not return the correct information.");
-        }
-    }
+    // public void isEcoFriendly(){
+    //     if (this.powerUsage <= 400){
+    //         println("The displayInfo method does not return the correct information.");
+    //     }
+    // }
     // isEcoFriendly Method
     // TODO: Implement a method to check if the appliance is eco-friendly based on its power usage. 
     // An appliance that uses less than 400 Watts of power is eco-friendly.
