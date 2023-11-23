7e7b819df9afad906e5dfdcd08cde08910126b75
jinasun <ginasuns60916@gmail.com>
2023-11-23 10:34:44 +0800

    refv2

--- src/main/java/com/example/Refrigerator.java
+++ src/main/java/com/example/Refrigerator.java
@@ -5,10 +5,13 @@ package com.example;
 public class Refrigerator extends Appliance{
     // Additional attributes
     private int temperature;
+    private String[]
+    private String foodItem;
 
     public Refrigerator(String brand, String model, int powerUsage, int temperature){
         super(brand, model,powerUsage);
         this.temperature = temperature;
+        this.foodItem = foodItem;
     }
     // Constructor
     // TODO: Initialize the Refrigerator with brand, model, power usage, temperature
@@ -18,8 +21,8 @@ public class Refrigerator extends Appliance{
     }
     // setTemperature Method
     // TODO: Implement a method to set the temperature of the refrigerator
-    
 
+    
     // Override displayInfo Method from Appliance
     // TODO: Implement a method that extends the information display to include the refrigerator's specific details (temperature)
 
@@ -30,10 +33,14 @@ public class Refrigerator extends Appliance{
     // Optional Bonus Tasks for additional marks:
     // =========================================
 
+    
     // TODO (Bonus): Add two additional attributes: `foodItems` (a String array) and `itemCount` (an int).
     // `foodItems` will hold the names of food items in the refrigerator, and `itemCount` will track the number of items.
     // Initialize `foodItems` to a length of 5 in the constructor and `itemCount` to 0.
 
+    public addFoodItem(){
+        for ()
+    }
     // TODO (Bonus): Write the `addFoodItem(String foodItem)` method.
     // This method should add a new food item to the refrigerator if it is possible. Check the test file to ensure you have the correct implementation.
     // If the item was added, the method should return true. Otherwise false.
